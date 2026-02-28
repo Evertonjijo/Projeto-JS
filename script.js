33e39b4f421517bc15515d4a0532159931435dbe
Everton Rodrigues <e_desouzarodrigues@fanshaweonline.ca>
2026-02-28 05:42:12 -0500

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Scroll suave nos links do menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
